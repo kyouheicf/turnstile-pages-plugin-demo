81f2519bb27dd8b038d91fff22e0fddfa102d5cd
kyouheicf <85217388+kyouheicf@users.noreply.github.com>
2024-03-08 11:58:44 +0900

    Validate the token

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -1,29 +1,40 @@
 /**
  * POST /api/submit
  */
-export async function onRequestPost({ request }) {
-	try {
-		let input = await request.formData();
 
-		// Convert FormData to JSON
-		// NOTE: Allows mutliple values per key
-		let tmp, output = {};
-		for (let [key, value] of input) {
-			tmp = output[key];
-			if (tmp === undefined) {
-				output[key] = value;
-			} else {
-				output[key] = [].concat(tmp, value);
-			}
+import turnstilePlugin from "@cloudflare/pages-plugin-turnstile";
+
+// This is a demo secret key. In prod, we recommend you store
+// your secret key(s) safely. 
+const SECRET_KEY = env.SECRET_KEY;
+
+export const onRequestPost = [
+    turnstilePlugin({
+    	secret: SECRET_KEY,
+    }),
+    (async (context) => {
+    	// Request has been validated as coming from a human
+    	const formData = await context.request.formData()
+
+    	var tmp, outcome = {};
+	for (let [key, value] of formData) {
+		tmp = outcome[key];
+		if (tmp === undefined) {
+			outcome[key] = value;
+		} else {
+			outcome[key] = [].concat(tmp, value);
 		}
-
-		let pretty = JSON.stringify(output, null, 2);
-		return new Response(pretty, {
-			headers: {
-				'Content-Type': 'application/json;charset=utf-8'
-			}
-		});
-	} catch (err) {
-		return new Response('Error parsing JSON content', { status: 400 });
 	}
-}
+
+	// Attach Turnstile outcome to the response
+	outcome["turnstile_outcome"] = context.data.turnstile;
+
+	let pretty = JSON.stringify(outcome, null, 2);
+
+      	return new Response(pretty, {
+      		headers: {
+      			'Content-Type': 'application/json;charset=utf-8'
+      		}
+      	});
+    })
+];
