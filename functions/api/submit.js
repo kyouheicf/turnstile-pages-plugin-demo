619d979b1d44670156527e25491d3aa9117db0e5
kyouheicf <85217388+kyouheicf@users.noreply.github.com>
2024-03-08 13:12:33 +0900

    Validate the token

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -13,6 +13,7 @@ export const onRequestPost = [
 			console.log(SECRET_KEY)
 			//turnstilePlugin({secret: SECRET_KEY}),
 			turnstilePlugin({secret: SECRET_KEY});
+			context.data.turnstile
 		}),
     (async (context) => {
     	// Request has been validated as coming from a human
